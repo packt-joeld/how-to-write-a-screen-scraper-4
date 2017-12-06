18390c38fb322a7b158a4bb65d857c5837d2e8ce
packt-joeld <joeld@packtpub.com>
2017-12-06 16:37:49 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,16 +1,17 @@
-###############################################################################
-# START HERE: Tutorial 3: More advanced scraping. Shows how to follow 'next' 
-# links from page to page: use functions, so you can call the same code 
-# repeatedly. SCROLL TO THE BOTTOM TO SEE THE START OF THE SCRAPER.
-###############################################################################
-
+##################################################################
+START HERE: Tutorial 3: More advanced scraping. Shows how to 
+follow 'next' links from page to page: use functions, so you can 
+call the same code repeatedly. SCROLL TO THE BOTTOM TO SEE THE 
+START OF THE SCRAPER.
+##################################################################
 import scraperwiki
 import urlparse
 import lxml.html
 
-# scrape_table function: gets passed an individual page to scrape
+# scrape_table function: gets passed an individual page to scrape 
 def scrape_table(root):
-    rows = root.cssselect("table.data tr")  # selects all <tr> blocks within <table class="data">
+    rows = root.cssselect("table.data tr")  # selects all <tr> 
+blocks within <table class="data">
     for row in rows:
         # Set up our data record - we'll need it later
         record = {}
@@ -22,11 +23,12 @@ def scrape_table(root):
             record['Sales m'] = table_cells[4].text
             # Print out the data we've gathered
             print record, '------------'
-            # Finally, save the record to the datastore - 'Artist' is our unique key
+            # Finally, save the record to the datastore - 'Artist'
+            #is our unique key
             scraperwiki.datastore.save(["Artist"], record)
-        
-# scrape_and_look_for_next_link function: calls the scrape_table
-# function, then hunts for a 'next' link: if one is found, calls itself again
+# scrape_and_look_for_next_link function: calls the scrape_table 
+function, then hunts for a 'next' link: if one is found, calls 
+itself again
 def scrape_and_look_for_next_link(url):
     html = scraperwiki.scrape(url)
     print html
@@ -35,14 +37,14 @@ def scrape_and_look_for_next_link(url):
     next_link = root.cssselect("a.next")
     print next_link
     if next_link:
-        next_url = urlparse.urljoin(base_url, next_link[0].attrib.get('href'))
+        next_url = urlparse.urljoin(base_url,
+        next_link[0].attrib.get('href'))
         print next_url
         scrape_and_look_for_next_link(next_url)
-
-# ---------------------------------------------------------------------------
-# START HERE: define your starting URL - then 
-# call a function to scrape the first page in the series.
-# ---------------------------------------------------------------------------
+# ----------------------------------------------------------------
+# START HERE: define your starting URL - then
+# call a function to scrape the first page in the series
+# ----------------------------------------------------------------
 base_url = 'http://www.madingley.org/uploaded/'
 starting_url = urlparse.urljoin(base_url, 'example_table_1.html')
 scrape_and_look_for_next_link(starting_url)
